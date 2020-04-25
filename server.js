c6ae891fa6f2802ade34ae3d588032ec1a4bda1c
Ben Loney <Loney@Bens-MacBook-Pro.local>
2020-04-25 11:03:48 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
-
 var db = require ("../models");
+
+var app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+var PORT = process.env.PORT || 3000;
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("App now listening on port:", PORT);
+    });
+});
