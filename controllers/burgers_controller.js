3862c8afd40909890c3f31e9752f42c3fc3a57a1
loneyb11 <benloney@gmail.com>
2020-04-28 12:43:28 -0500

    progress

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,28 +1,31 @@
 var express = require("express");
 var burger = require ("../models/burger.js");
-
+var viewObj = { burgers: data };
 
 var router = express.Router();
 
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
-        var viewObj = { burgers: data };
         res.render("index", viewObj);
     });
 });
 
 router.post("/burgers/create", function(req, res) {
-    console.log("creating a burger with new name: " + req.body.burger_name);
-    burger.insertOne("burger_name", req.body.name, function(result) {
-
-        res.json("result");
+    console.log("creating a burger with new name: ");
+    burger.insertOne("burger_name", req.body.name, function() {
+    
+        res.render("index", viewObj);
          
     });
 });
 
 router.put("/burgers/update/devour/:id", function(req, res) {
     burger.updateOne("burgers", "devoured", req.params.id, function() {
-        res.redirect("/burgers");
+        if (result.changedRows == 0) {
+            res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
     });
 });
 
